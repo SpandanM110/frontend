e1c2302d4de5a607b320c9fc036a1abaea0452e9
mohdshahzil <theshahzil@gmail.com>
2024-02-26 02:25:41 +0530

    next-ui-integrated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Providers } from "./providers";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
+const { nextui } = require("@nextui-org/react");
 import type { Config } from "tailwindcss";
-
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -15,6 +16,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
